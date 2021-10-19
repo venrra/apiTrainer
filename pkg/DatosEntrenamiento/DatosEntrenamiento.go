98ae930000bef2c201a1e89b70b8cd4f3f6ba7f2
Jose Antonio Fernandez Vilchez <90417705+josevilchez247@users.noreply.github.com>
2021-10-19 18:53:49 +0200

    Quitamos la funcion pulsaciones por minuto
    Los datos recibidos no necesitan transformación de unidad

--- pkg/DatosEntrenamiento/DatosEntrenamiento.go
+++ pkg/DatosEntrenamiento/DatosEntrenamiento.go
@@ -5,10 +5,3 @@ type DatosEntrenamiento struct {
 	tiempo          []int
 }
 
-func (datos DatosEntrenamiento) calcular_PulsacionesPorMinuto() (media int) {
-	for i := 0; i < len(datos.hr_recuperacion); i++ {
-		for j := 0; j < len(datos.tiempo); j++ {
-
-		}
-	}
-}
